b84490132ad3ea5c1d19d7ed53853d7534f66359
Pablo Lechon <plechon@uchicago.com>
2025-04-18 12:48:35 -0500

    small update

--- code/hidden_states/minimal_test_glv_chaotic.py
+++ code/hidden_states/minimal_test_glv_chaotic.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 # ----------------------
 # Step 1: Load data from CSV
 # ----------------------
-data = pd.read_csv("hudson-bay-lynx-hare.csv")
+data = pd.read_csv("predator_prey.csv")
 #data = data.iloc[0:50,:]
+#data = data.iloc[30:,:]
 nspp = np.shape(data)[1]-1
 ntpoints = np.shape(data)[0]
 time = data.iloc[:, 0].to_numpy()
@@ -23,12 +24,12 @@ proportions = species / species_sum
 # Prepare derivatives
 dp_all = np.gradient(proportions, axis=0) / (time[1] - time[0])
 
-pad = 4
+pad = 3
 p_obs = proportions[pad:-pad]      # (T-2*pad, 4)
 dp_obs = dp_all[pad:-pad]          # (T-2*pad, 4)
 
 # Create sliding window input for encoder
-window_size = 9
+window_size = 7
 p_windows = np.stack([proportions[i - pad:i + pad + 1] for i in range(pad, len(proportions) - pad)])  # (T-2*pad, 9, 4)
 
 # ----------------------
@@ -36,11 +37,12 @@ p_windows = np.stack([proportions[i - pad:i + pad + 1] for i in range(pad, len(p
 # ----------------------
 def encoder_fn(x):
     return hk.Sequential([
-        hk.Conv1D(32, kernel_shape=9, padding="VALID"),
-        jax.nn.relu,
-        hk.Conv1D(32, kernel_shape=1),
-        jax.nn.relu,
+        hk.Conv1D(8, kernel_shape=7, padding="VALID"),
+        jax.nn.tanh,
         hk.Conv1D(1, kernel_shape=1),
+        jax.nn.softplus,
+        #hk.Conv1D(1, kernel_shape=1),
+        #jax.nn.relu,
     ])(x)
 
 encoder = hk.without_apply_rng(hk.transform(encoder_fn))
@@ -62,7 +64,7 @@ def symbolic_model(p, N, W):
         #(p**2) * N_col,            # (T, 4)
         p * (N_col**2),            # (T, 4)
         #N_col**3                   # (T, 1)
-        (p**2)*(N_col)**2
+        (p**2)*(N_col)**2,
         #p*(N_col**3), 
         #(p**2)*(N_col**3),
         #(p**3)*(N_col**3)
@@ -77,7 +79,7 @@ def loss_fn(params, W, x, p_mid, dp_true):
     dp_pred = symbolic_model(p_mid, N_pred, W)  # (T, 4)
     mse = jnp.mean((dp_pred - dp_true)**2)
     sparsity = jnp.sum(jnp.abs(W))
-    return mse + 1e-8 * sparsity
+    return mse #+ 1e-5* sparsity
 
 optimizer = optax.adam(1e-3)
 
@@ -101,7 +103,7 @@ params = encoder.init(jax.random.PRNGKey(42), p_windows)
 W = jnp.zeros((6, nspp))  # one equation per species
 opt_state = optimizer.init((params, W))
 
-for step in range(5000):
+for step in range(10000):
     params, W, opt_state, loss = update(params, W, opt_state, p_windows, p_obs, dp_obs)
     if step % 500 == 0:
         print(f"Step {step}, Loss: {loss:.5f}")
